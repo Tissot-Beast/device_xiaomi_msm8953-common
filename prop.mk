33309aeb2bdc162bc6cc5c171818cc9cb71f6c65
mdeejay <mdjrussia@gmail.com>
2018-12-20 11:32:10 +0100

    msm8953-common: Add WFD

--- prop.mk
+++ prop.mk
@@ -71,7 +71,6 @@ debug.egl.hw=0 \
 debug.enable.sglscale=1 \
 debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
-debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
@@ -83,8 +82,13 @@ ro.sf.lcd_density=440 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.display.enable_default_color_mode=1
+
+# WFD
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.enable_hwc_vds=1 \
+vendor.gralloc.enable_fb_ubwc=1 \
+vendor.gralloc.disable_wb_ubwc=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
