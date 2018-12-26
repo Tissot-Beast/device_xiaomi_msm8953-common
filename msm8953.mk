ace540db7ed2dc13ca9fd1e2f02eb898b6d39522
mdeejay <mdjrussia@gmail.com>
2018-12-26 18:28:24 +0100

    msm8953-common: Import legacy USB HAL

--- msm8953.mk
+++ msm8953.mk
@@ -346,7 +346,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_8953
 
 # VNDK
 PRODUCT_PACKAGES += \
