dec6f5ee6956ee04b8cff5df7d6befacdd4514bb
mdeejay <mdjrussia@gmail.com>
2019-02-06 03:12:33 +0100

    msm8953-common: Cleanups camera packages

--- msm8953.mk
+++ msm8953.mk
@@ -125,12 +125,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Gcam \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    Gcam
 
 # Configstore
 PRODUCT_PACKAGES += \
